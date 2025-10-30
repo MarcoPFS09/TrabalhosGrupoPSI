1b3dfcb16d8dc4fc09e63d1b4db319af94ee33ef
Marco Santos <mpfs122009@gmail.com>
2025-10-30 11:48:34 +0000

    ...

--- TrabGrupoM9_Pert/Program.cs
+++ TrabGrupoM9_Pert/Program.cs
@@ -10,36 +10,26 @@ namespace TrabGrupoM9_Pert
     {
         static void Main(string[] args)
         {
-            int N = 10;
-            int[] vetor = new int[N];
-
-            Console.WriteLine($"Digite {N} números inteiros (aperte Enter após cada um):");
-            for (int i = 0; i < N; i++)
-            {
-                Console.Write($"{i + 1}: ");
-                // tenta ler e converter; repete até valor válido
-                while (!int.TryParse(Console.ReadLine(), out vetor[i]))
-                {
-                    Console.Write("Entrada inválida. Digite um número inteiro: ");
-                }
-            }
-
-            int indiceMaior = 0;
-            int valorMaior = vetor[0];
-
-            for (int i = 1; i < N; i++)
-            {
-                if (vetor[i] > valorMaior)
-                {
-                    valorMaior = vetor[i];
-                    indiceMaior = i;
-                }
-            }
-
             Console.WriteLine();
-            Console.WriteLine($"Maior valor: {valorMaior}");
-            Console.WriteLine($"Índice (base 0): {indiceMaior}");
-            Console.WriteLine($"Índice (base 1): {indiceMaior + 1}");
+            int[] numeros = new int[10];
+            Console.WriteLine($"Digite números inteiros: ");
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.Write($"{i+1}-");
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+            int maximo = numeros[0];
+            int indice = 1;
+            for (int i = 1; i < numeros.Length; i++)
+            {
+                if (numeros[i] > maximo)
+                {
+                    maximo = numeros[i];
+                    indice = i;
+                }
+            }
+            Console.WriteLine($"Maior número: {maximo}, índice: {indice + 1}");
+            Console.ReadKey(); 
         }
     }
 }
